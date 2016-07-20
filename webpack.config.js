ccb76c8ca9add75bc8976078d61c63f39da9b201
maccana <mikeymccann@gmail.com>
2016-07-20 17:41:34 -0400

    Change title property in HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack demo'
+      title: 'Webpack Starter'
     }),
     new ExtractTextPlugin("style.css", {allChunks: false} )
 
